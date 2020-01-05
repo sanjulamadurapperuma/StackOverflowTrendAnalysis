54fb5c6890344fc5ede37bb36964be1a807428df
Sanjula Madurapperuma <sanjula99@gmail.com>
2020-01-05 20:23:22 +0530

    Adding imports and initializing matplotlib parameters

---
+++ TrendAnalysis.py
@@ -0,0 +1,16 @@
+# Adding all the imports
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+import json
+
+# Initializing matplotlib parameters
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
