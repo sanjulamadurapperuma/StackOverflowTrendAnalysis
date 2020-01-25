70cd89f2134c4a7c75066e3e2a6d37cd8ef6eda5
Sanjula Madurapperuma <sanjula99@gmail.com>
2020-01-25 14:11:02 +0530

    Removing google drive url and adding relative path

--- TrendAnalysis.py
+++ TrendAnalysis.py
@@ -1,12 +1,12 @@
 # Adding all the imports
-import warnings
-import itertools
-import numpy as np
-import matplotlib.pyplot as plt
+# import warnings
+# import itertools
+# import numpy as np
+# import matplotlib.pyplot as plt
 import pandas as pd
-import statsmodels.api as sm
+# import statsmodels.api as sm
 import matplotlib
-import json
+# import json
 
 # Initializing matplotlib parameters
 matplotlib.rcParams['axes.labelsize'] = 14
@@ -14,6 +14,13 @@ matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
 matplotlib.rcParams['text.color'] = 'k'
 
-# URL for the main dataset in Google Drive
-# TODO - Replace the url with local path
-url = 'https://drive.google.com/open?id=1KhRlMBLA4I6G08nqjH63fOBiSxz5WBzV'
+# url = 'https://drive.google.com/open?id=1KhRlMBLA4I6G08nqjH63fOBiSxz5WBzV'
+path = 'data/StackOverflowTagsUpdated.csv'
+
+data = pd.read_csv(path, parse_dates=['date'])
+# Dataset is now stored in a Pandas Dataframe
+
+# Print overview of the dataset
+data.head()
+
+data.describe()
